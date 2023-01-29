d1d4eb4ec444048ac809815ff59dfbaa71b345f3
jeremygardel <jeremy-asklany@proton.me>
2023-01-30 00:01:12 +0200

    comments

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
-/* TEEEST */
+ /* TEEEST */
 
 var clickreply = document.getElementById("btnp");
 var clicklike = document.getElementById("clickme");
+var divy = document.getElementById("divs");
 
-
-clickreply.onclick = function () {
-    divs.style.display = "flex";
+/*  Here when we define the function 'onclickreply' and ask if its none or not...' */
+function onclickreply() {
+if (divy.style.display === "none") {
+    divy.style.display = "flex";
+  } else {
+    divy.style.display = "none";
+  }
 }
 
+clickreply.onclick = onclickreply;  // here onclick on clickreply button, go to function click here so its shows at first click and hidden at second click
 
-clicklike.onclick = function () {
-    clickme.style.fontWeight= "700";
-    console.log(clicklike);
-    clickme.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>Liked';
+
+clicklike.onclick = function onclicklike () {
+    clickme.style.fontWeight= "700"; // This is for Font Weight
+    clickme.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>Liked'; // This is for Viewing the icon with the letter
+    console.log(clicklike); 
 };
