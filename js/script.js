48cb4400e3c30c62e6a2f30bf9ffc35dae59efb7
jeremygardel <jeremy-asklany@proton.me>
2023-01-29 23:21:36 +0200

    Started Simple Java

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* TEEEST */
+
+var clickreply = document.getElementById("btnp");
+var clicklike = document.getElementById("clickme");
+
+
+clickreply.onclick = function () {
+    divs.style.display = "flex";
+}
+
+
+clicklike.onclick = function () {
+    clickme.style.background = "grey";
+    console.log(clicklike);
+};
