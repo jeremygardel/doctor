c42ec1a32320ad53a8ca5c38b325fd977ffbd415
jeremygardel <jeremy-asklany@proton.me>
2023-01-29 23:41:46 +0200

    Simple OnClick

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ clickreply.onclick = function () {
 
 
 clicklike.onclick = function () {
-    clickme.style.background = "grey";
+    clickme.style.fontWeight= "700";
     console.log(clicklike);
+    clickme.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>Liked';
 };
