968e24b4d418a553623db4ad10d779483b04fc43
jeremygardel <jeremy-asklany@proton.me>
2023-02-08 23:35:12 +0200

    Testing JS Edit

--- Testing Room/js/testing.js
+++ Testing Room/js/testing.js
@@ -1,29 +1,65 @@
+// Java Script Testing
+// Dynamic 
+
+
 let title = " Zero" , desc = " El zero web school" , date = 2022;
 
 let a = 2;
 let b = 1;
-console.log(b - a);
+
 let div = `
     <div>
       <h3>${title}</h3>
       <p> ${desc}</p>
-      <span>${date}</span>
+      <span> ${date}</span>
     </div>
 
 `;
-
-let numberBe = "mahmoud web schools";
-
-
 document.write(div.repeat(2)); // Repeat
 
-document.write(numberBe.charAt(14).toUpperCase().repeat(2)); // 8H
 
-console.log(numberBe.slice(0 , 7).charAt(0).toUpperCase() + numberBe.slice(1,7));  //  Mahmoud
+// If condition 
 
-console.log(numberBe.split(" ", 1));  // Return one array
+let r = 40;
+  // IF CONDITION Ternary
+r < 10 ? console.log(10)  : 
+  r >= 10 && r <= 40 ? console.log("10 to  40") :
+  r > 40 ? console.log("bigger than 40") :
+  console.log("not found");
 
-console.log(numberBe.substr(0, 7) + " " + numberBe.substr(12) );   // Mahmoud Schools with Concentation 
+ 
+
+
+// Concentation & String Methods
+let numberBe = "mahmoud web schools";
+if(numberBe.length + numberBe.length === 37 )
+{
+  console.log(numberBe.length + "Found");
+}
+else { console.log("UNDEFINED");
+}
+
+if(numberBe.charAt(numberBe.search("w")) === "w") {
+	console.log("Good Searched for W");
+}
+
+if(typeof numberBe === "String")
+{
+  console.log("Yes sir ");
+}
+else {
+  console.log("a7a");
+}
+
+document.write(numberBe.charAt(14).toUpperCase().repeat(7)); // 8H
+
+console.log(numberBe.slice(0 ,1).toUpperCase() + numberBe.slice(1,7));  //  Mahmoud
+
+console.log(numberBe.split(" ", 1));  // Return one array (split for array)
+
+console.log(numberBe.substr(0, 7) + " " + numberBe.substr(12, 7) );   // Mahmoud Schools     with Concentation 
 
 console.log(numberBe.slice(0, 1).toLowerCase() + numberBe.slice(1,18).toUpperCase()
  + numberBe.slice(-1).toLowerCase());   // mAHMOUD WEB SCHOOLs 
+
+
